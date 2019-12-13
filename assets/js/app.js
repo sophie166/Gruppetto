bbc15497c84119bfa8a0f3d82876e384b1ca059b
batryad <55141444+batryad@users.noreply.github.com>
2019-12-13 17:51:14 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,13 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
-
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
-
-
-
 // hover & click for the FAQ
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
