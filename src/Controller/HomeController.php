b5949f9b804ab768d21cc015952fefe155554924
gregoire masson <g.masson279@gmail.com>
2020-01-06 09:37:26 +0100

    resolve conflit of booking class commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+    /**
+     * @Route("/navbar", name="home")
+     */
+    // To display the navbar we be remove when we create the other page //
+    public function nav()
+    {
+        return $this->render('navbar/navbar.html.twig');
+    }
+}
