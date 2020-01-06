468a39e350e7093c7684b55fb20b01993d45d0f1
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-06 18:00:27 +0100

    First tape add fixture on event page

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+         $event = new Event();
+         $event->setNameEvent('Entrainement de course ');
+         $event->setLevelEvent('Debutant');
+         $event->setDateEvent('XXXX');
+         $event->setTimeEvent('1:12');
+         $event->setDescription('Courses dans la nature');
+         $event->setParticipantLimit('10');
+        $manager->persist($event);
+
+        $manager->flush();
+    }
+}
