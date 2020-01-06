c6af4806648fcbfe5ce1e81055d7b585a545dc2a
batryad <ryadus2001@gmail.com>
2020-01-06 11:06:52 +0100

    debug de JS de la FAQ

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,14 +14,15 @@ require('../scss/app.scss');
 // FAQ scroll through the answers.
 const acc = document.getElementsByClassName('accordion');
 let i;
+function faqList() {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = `${panel.scrollHeight}px`;
+    }
+}
 for (i = 0; i < acc.length; i += 1) {
-    acc[i].addEventListener('click', () => {
-        this.classList.toggle('active');
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = `${panel.scrollHeight}px`;
-        }
-    });
+    acc[i].addEventListener('click', faqList);
 }
