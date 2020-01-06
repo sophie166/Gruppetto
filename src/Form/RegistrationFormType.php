44a86d4742dda000a1c31634816c6e7fd40c6826
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-06 13:02:22 +0100

    adding constraints to password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -13,6 +13,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -38,7 +39,12 @@ class RegistrationFormType extends AbstractType
                 'label'=>'En m’inscrivant, je certifie avoir lu et accepté les Conditions Générales d’Utilisation'
             ])
             ->add('plainPassword', RepeatedType::class, [
-                'constraints' => [new Length(['min' => 3]), new NotBlank()],
+                'constraints' => [new Length(['min' => 6]), new NotBlank(), new Regex([
+                    //at least one uppercase letter, one lowercase letter, one number and one special character
+                    'pattern' => '#^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$#',
+                    'message' => "Votre mot de passe doit contenir au moins 6 charactères dont au moins une majuscule,
+                     une minuscule, un chiffre et un charactère spécial"
+                ])],
                 'type'=>PasswordType::class,
                 'mapped' => false,
                 'invalid_message'=>'Passwords must match',
