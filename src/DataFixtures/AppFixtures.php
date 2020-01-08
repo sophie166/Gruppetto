78331488c96d9fd0cc05502797e5ab496f2c4781
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-08 15:12:31 +0100

    bug on this branch, i have to create a special fixture branch

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,14 +11,15 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        // Fixtures for event page//
          $event = new Event();
          $event->setNameEvent('Entrainement de course ');
-         $event->setLevelEvent('Debutant');
+         $event->setLevelEvent(1);
          $event->setDateEvent(new\ DateTime(6/12/2019));
          $event->setTimeEvent(new\ DateTime(16/44/12));
          $event->setDescription('Courses dans la nature');
          $event->setParticipantLimit('10');
-         $event->setPlaceEvent('23 place des écoliers 59000 Lille');
+         $event->setPlaceEvent('23 place des ecoliers 59000 Lille');
         $manager->persist($event);
 
         $manager->flush();
