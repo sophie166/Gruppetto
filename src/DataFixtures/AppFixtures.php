84f9df08f5e7595ed5c076eafc80b4f792de65e2
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-08 16:13:54 +0100

    fixtures, pull for migration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use App\Entity\ProfilClub;
+use App\Entity\ProfilSolo;
+use App\Entity\Sport;
+use App\Entity\SportCategory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -11,6 +15,40 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        // fFixures for profil club//
+        $profilClub = new ProfilClub();
+        $profilClub->setNameClub('Run Team');
+        $profilClub->setCityClub('Lille');
+        $profilClub->setLogoClub('avatar2.jpg');
+        $profilClub->setDescriptionClub('Petite equipe Liloise');
+        $manager->persist($profilClub);
+
+        // Fixtures for profil Solo//
+        $profilSolo= new ProfilSolo();
+        $profilSolo->setLastname('Doe');
+        $profilSolo->setFirstname('Jonh');
+        $profilSolo->setBirthdate(new\ DateTime(141220));
+        $profilSolo->setDescription('My description');
+        $profilSolo->setGender(0);
+        $profilSolo->setAvatar('avatar.jpg');
+        $profilSolo->setEmergencyContactName('Pascale Dino');
+        $profilSolo->setLevel(1);
+        $profilSolo->setSportFrequency(2);
+        $profilSolo->setPhone('0000000000');
+        $profilSolo->setEmergencyPhone('0000000000');
+        $manager->persist($profilSolo);
+
+        // Fixtures for sportCategory//
+        $sportCategory=new SportCategory();
+        $sportCategory->setNameCategory('Running');
+        $manager->persist($sportCategory);
+
+        // Fixtures for sport//
+        $sport= new Sport();
+        $sport->setSportName('Courses');
+        $sport->setSportCategory($sportCategory);
+        $manager->persist($sport);
+
         // Fixtures for event page//
          $event = new Event();
          $event->setNameEvent('Entrainement de course ');
@@ -20,6 +58,10 @@ class AppFixtures extends Fixture
          $event->setDescription('Courses dans la nature');
          $event->setParticipantLimit('10');
          $event->setPlaceEvent('23 place des ecoliers 59000 Lille');
+         $event->setSport($sport);
+         $event->setCreatorClub($profilClub);
+         $event->setCreatorSolo($profilSolo);
+
         $manager->persist($event);
 
         $manager->flush();
