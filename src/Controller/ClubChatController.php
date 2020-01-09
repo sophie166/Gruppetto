bf32a43cda70d884ef1d1c264acfff21eda3a8c2
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-09 15:21:14 +0100

    integration general chat

--- src/Controller/ClubChatController.php
+++ src/Controller/ClubChatController.php
@@ -3,17 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ClubChatController
+ * @package App\Controller
+ * @Route("/club/chat", name="club_chat")
+ */
 class ClubChatController extends AbstractController
 {
     /**
-     * @Route("/club/chat", name="club_chat")
+     * @Route("", name="")
+     * @return Response
      */
-    public function index()
+    public function chat(): Response
     {
         return $this->render('club_chat/index.html.twig', [
-            'controller_name' => 'ClubChatController',
         ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/general", name="_general")
+     */
+    public function chatGeneral() : Response
+    {
+        return $this->render('club_chat/general.html.twig');
     }
 }
