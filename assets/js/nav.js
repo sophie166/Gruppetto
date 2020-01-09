c29aa75d24f25f655afba06803a4d11a59290698
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-09 11:12:59 +0100

    modification navBar

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+// Interact with navbar, swipe effect !
+const myArrow = document.querySelector('#arrow-swipe');
+const myHeader = document.querySelector('header');
+
+
+myArrow.addEventListener('click', () => {
+    myHeader.classList.toggle('closed');
+});
