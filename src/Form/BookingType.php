ef39397aa512c09c33de48e8c95c16cb9417fac9
Toure18 <tmadjoula@gmail.com>
2020-01-10 14:09:48 +0100

    form of events

---
+++ src/Form/BookingType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('beginAt', ButtonType::class)
+            ->add('beginAt')
+            ->add('endAt', ButtonType::class)
+            ->add('title', ButtonType::class )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
