9b4b0ab6025c0389d1620d59c4f8a3a23e56719d
Toure18 <tmadjoula@gmail.com>
2020-01-10 14:05:19 +0100

    controler of calendar

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,112 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Booking;
+use App\Form\BookingType;
+use App\Repository\BookingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookingController extends AbstractController
+{
+
+
+    /**
+     * @Route("/booking/new", name="booking_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($booking);
+            $entityManager->flush();
+            return $this->redirectToRoute('booking_calendar');
+        }
+        return $this->render('booking/new.html.twig', [
+            'booking' => $booking,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/booking/show/{id}", name="booking_show", methods={"GET"})
+     * @param BookingRepository $bookingRepository
+     * @param Booking $booking
+     * @return Response
+     */
+    public function show(BookingRepository $bookingRepository, booking $booking): Response
+    {
+        return $this->render('booking/show.html.twig', [
+            'bookings' => $bookingRepository->findAll(),
+            'booking' => $booking
+        ]);
+    }
+
+    /**
+     * @Route("/booking/edit/{id}", name="booking_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Booking $booking
+     * @return Response
+     */
+    public function edit(Request $request, Booking $booking): Response
+    {
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('booking_index');
+        }
+        return $this->render('booking/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/booking/delete/{id}", name="booking_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Booking $booking
+     * @return Response
+     */
+    public function delete(Request $request, Booking $booking): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$booking->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($booking);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('booking_index');
+    }
+
+    /**
+     * @Route("/booking/calendar", name="booking_calendar", methods={"GET"})
+     * @param BookingRepository $bookingRepository
+     * @return Response
+     */
+
+    public function calendar(BookingRepository $bookingRepository): Response
+    {
+        return $this->render('booking/calendar.html.twig', [
+            'bookings' => $bookingRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/booking", name="booking_index", methods={"GET"})
+     * @param BookingRepository $bookingRepository
+     * @return Response
+     */
+    public function index(BookingRepository $bookingRepository): Response
+    {
+        return $this->render('booking/index.html.twig', [
+            'bookings' => $bookingRepository->findAll(),
+        ]);
+    }
+}
