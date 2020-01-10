b46ab2beacce705d34fe4f5fa08211ef90f7be30
Gregoire Masson <55199961+provencal91@users.noreply.github.com>
2020-01-10 15:28:43 +0100

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -38,7 +38,6 @@ class RegistrationController extends AbstractController
             // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_profil_register');
-
         }
 
         return $this->render('registration/register.html.twig', [
