4a7513ec2cc8aecdc06ad3788523d1c343ca80b1
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-10 11:26:20 +0100

    installing faker, begin soloFixtures

---
+++ src/DataFixtures/ProfilSoloFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use App\Entity\ProfilSolo;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ProfilSoloFixtures
+{
+
+
+}
