a6fdb1e6662907b1a0c2af65a6bbe6ac8d964b98
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-11 17:40:14 +0100

    add icons, wor on some display un scss

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('nav', './assets/js/nav.js')
     .addEntry('messages', './assets/js/messages.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('profil', './assets/js/profil.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
