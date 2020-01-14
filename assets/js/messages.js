18935e93e10d8902ab36c5d97495c8df83c1ed6f
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-14 14:59:56 +0100

    travis error jquery

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,6 +1,9 @@
 require('../scss/messages.scss');
 require('../js/searchBar');
-document.head.innerHTML += "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>";
+var sc = document.createElement("script");
+sc.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+sc.setAttribute("type", "text/javascript");
+document.head.appendChild(sc);
 // scrolls the chatbox to the bottom
 $(document).ready(() => {
     $('.js-chatbox').animate({ scrollTop: $('.js-chatbox').get(0).scrollHeight }, -2000);
