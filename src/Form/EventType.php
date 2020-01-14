46120aef0f0991224215ae50d749f700736ad9fb
Toure18 <tmadjoula@gmail.com>
2020-01-14 17:43:13 +0100

    form of event

---
+++ src/Form/EventType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nameEvent')
+            ->add('levelEvent')
+            ->add('dateEvent')
+            ->add('timeEvent')
+            ->add('description')
+            ->add('participantLimit')
+            ->add('placeEvent')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
