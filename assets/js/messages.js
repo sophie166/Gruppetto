3f9f5894cd832b3f17aacfa4812f58664911e323
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-14 09:44:12 +0100

    correcting for PR

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,6 +1,7 @@
 require('../scss/messages.scss');
 require('../js/searchBar');
 
+// scrolls the chatbox to the bottom
 $(document).ready(() => {
     $('.js-chatbox').animate({
         scrollTop: $('.js-chatbox').get(0).scrollHeight
