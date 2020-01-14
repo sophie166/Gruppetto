53773fa185ad20ce4eae66a025d470d2f6ae89ec
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-14 14:29:15 +0100

    creation of service to get club entity from logged user

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,9 +1,7 @@
 require('../scss/messages.scss');
 require('../js/searchBar');
-
+require('jquery-3.4.1.min.js');
 // scrolls the chatbox to the bottom
 $(document).ready(() => {
-    $('.js-chatbox').animate({
-        scrollTop: $('.js-chatbox').get(0).scrollHeight
-    }, -2000);
+    $('.js-chatbox').animate({ scrollTop: $('.js-chatbox').get(0).scrollHeight }, -2000);
 });
