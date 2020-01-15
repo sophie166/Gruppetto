22749ab36ccf7a4fe8d75ef85f72a2655fb87f6b
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-15 16:52:01 +0100

    add css

--- src/Form/ProfilClubType.php
+++ src/Form/ProfilClubType.php
@@ -8,6 +8,7 @@ use phpDocumentor\Reflection\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
