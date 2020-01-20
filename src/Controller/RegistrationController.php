829d1736dc822a9bd6704f95b7595da753522348
Gregoire Masson <55199961+provencal91@users.noreply.github.com>
2020-01-20 14:41:12 +0100

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,7 +53,6 @@ class RegistrationController extends AbstractController
 
 
             return $this->render('profil/index.html.twig');
-
         }
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
