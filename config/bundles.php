aa858820e3efbf8c149fe5d2f5233ec74fe84ae4
Toure18 <tmadjoula@gmail.com>
2020-01-22 13:54:32 +0100

    configuration

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+use FOS\JsRoutingBundle\FOSJsRoutingBundle;
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
@@ -16,4 +18,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOSJsRoutingBundle::class => ['all' => true],
 ];
