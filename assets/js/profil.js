f06c217a29bfd3e56ec353a7b0f48ba32ea50f13
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-22 09:46:24 +0100

    fi travis erros

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -5,9 +5,9 @@ const myPens = document.querySelectorAll('.btn-modify');
 const myForms = document.querySelectorAll('.box-form-update');
 const myTexts = document.querySelectorAll('.box-text');
 
-for (let i=0; i<= myForms.length; i++) {
-    myPens[i].addEventListener('click',() => {
+for (let i = 0; i <= myForms.length; i+1) {
+    myPens[i].addEventListener('click', () => {
         myForms[i].classList.toggle('displayed');
         myTexts[i].classList.toggle('hidden');
-
-    })}
+    })
+}
