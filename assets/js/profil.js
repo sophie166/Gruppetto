dcd1bea026ca635976f4929cc814df911ac4c682
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-22 16:45:24 +0100

    fix bug

--- assets/js/profil.js
+++ assets/js/profil.js
@@ -5,7 +5,7 @@ const myPens = document.querySelectorAll('.btn-modify');
 const myForms = document.querySelectorAll('.box-form-update');
 const myTexts = document.querySelectorAll('.box-text');
 
-for (let i = 0; i <= myForms.length; i + 1) {
+for (let i = 0; i <= myForms.length; i += 1) {
     myPens[i].addEventListener('click', () => {
         myForms[i].classList.toggle('displayed');
         myTexts[i].classList.toggle('hidden');
