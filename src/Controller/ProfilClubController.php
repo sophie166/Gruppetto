b0733f6630a8d7b8b2a9cd441c6f75e03a25cf72
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2020-01-23 12:06:03 +0100

    remove all function from profil club controller and templates twig was not necesseray, add a clubLogo directory uploads

--- src/Controller/ProfilClubController.php
+++ src/Controller/ProfilClubController.php
@@ -19,52 +19,6 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProfilClubController extends AbstractController
 {
-    /**
-     * @Route("/", name="profil_club_index", methods={"GET"})
-     */
-    public function index(ProfilClubRepository $profilClubRepository): Response
-    {
-        return $this->render('profil_club/index.html.twig', [
-            'profil_clubs' => $profilClubRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="profil_club_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $profilClub = new ProfilClub();
-        $form = $this->createForm(ProfilClubType::class, $profilClub);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // upload logoform
-
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($profilClub);
-            $entityManager->flush();
-            return $this->redirectToRoute('profil_club_index');
-        }
-
-        return $this->render('profil_club/new.html.twig', [
-            'profil_club' => $profilClub,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /*/**
-     * @Route("/{id}", name="profil_club_show", methods={"GET"})
-     */
-    /*
-    public function show(ProfilClub $profilClub): Response
-    {
-        return $this->render('profil_club/show.html.twig', [
-            'profil_club' => $profilClub,
-        ]);
-    }
-    */
 
     /**
      * @Route("/{id}/", name="profil_club_edit", methods={"GET","POST"})
@@ -95,19 +49,5 @@ class ProfilClubController extends AbstractController
             'profil_club' => $profilClub,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="profil_club_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, ProfilClub $profilClub): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$profilClub->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($profilClub);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('profil_club_index');
     }
 }
