4aad9831050b71cc470fe3542e9f09e20fdd73bc
Toure18 <tmadjoula@gmail.com>
2020-01-23 18:33:36 +0100

    style of page events

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('new_events', './assets/js/new_events.js')
     .addEntry('edit_events', './assets/js/edit_events.js')
     .addEntry('details', './assets/js/details.js')
+    .addEntry('participation', './assets/js/participation.js')
 
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
